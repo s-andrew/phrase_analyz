3cb422586540957fd92a196c5a3dcd078b1e8b12
s-andrew <anso2393@yandex.ru>
2018-09-06 12:23:35 +0300

    Added PhrasePreprocessor

---
+++ PhrasePreprocessor.py
@@ -0,0 +1,38 @@
+import re
+import pymorphy2
+
+
+class PhrasePreprocessor:
+    def __init__(self, regex:list=None, min_max_word_len:tuple=(1, 10),
+                 min_max_phrase_len:tuple=(1, 100), stop_pos:list=None):
+        if regex is not None:
+            self.regex = list(map(lambda r: re.compile(r), regex))
+        else:
+            self.regex = []
+        self.morph = pymorphy2.MorphAnalyzer()
+        self.min_word_len, self.max_word_len = min_max_word_len
+        self.min_phrase_len, self.max_phrase_len = min_max_phrase_len
+        if stop_pos is not None:
+            self.stop_pos = stop_pos
+        else:
+            self.stop_pos = []
+
+    def normalize(self, text:str):
+        text = text.lower()
+        for r in self.regex:
+            text = r.sub(' ', text)
+        words = text.split()
+        words = map(lambda w: w.strip(), words)
+        words = filter(lambda w: len(w) >= self.min_word_len and len(w) <= self.max_word_len, words)
+        words = map(self.word_normalize, words)
+        words = filter(lambda wp: wp[1] not in self.stop_pos, words)
+        words = map(lambda wp: wp[0], words)
+        words = list(set(words))
+        return words
+
+    def word_normalize(self, word:str):
+        w = self.morph.parse(word)
+        w = w[0]
+        return w.normal_form, w.tag.POS
+
+
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from sqlalchemy_control import SqlalchemyControl
 from sqlalchemy_control.phrase_factory import phrase_consistency_factory
+from PhrasePreprocessor import PhrasePreprocessor
 
 if __name__ == '__main__':
     db = SqlalchemyControl('config/sqlite_connection.json', 'config/schema.json', create=True)
@@ -10,7 +11,7 @@ if __name__ == '__main__':
         "Hello, I love you, let me jump in your game"
     ]
 
-    phrases = [phrase_consistency_factory(text, 'PHRASE_OWNER_STUB', 666, session, commit=False) for text in phrases]
-    session.commit()
+    preprocessor = PhrasePreprocessor()
 
-    # print(phrases)
+    phrases = [phrase_consistency_factory(text, 'PHRASE_OWNER_STUB', 666, session, commit=False, preprocessor=preprocessor.normalize) for text in phrases]
+    session.commit()
